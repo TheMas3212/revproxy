07a5a63f1413fd6baace11b673ba39b013647131
TheMas3212 <mas3212@gmail.com>
2020-10-06 10:17:35 +1100

    - Add support for wildcards - Add support for no default route

--- src/index.ts
+++ src/index.ts
@@ -52,48 +52,62 @@ if (cluster.isMaster) {
   }
 
   app.disable("x-powered-by");
-  const mapping = new Map();
-  const defaultroute = proxy.createProxy({ target: DEFAULT_ROUTE, xfwd: true });
+  const mapping: Map<string,any> = new Map();
+  let defaultProxy;
+  if (DEFAULT_ROUTE !== 'none') {
+    defaultProxy = proxy.createProxy({ target: DEFAULT_ROUTE, xfwd: true });
+  } else {
+    defaultProxy = {
+      "web": (req, res) => {
+        res.send("Proxy Online");
+      }
+    };
+  }
+  
 
   for (const item of jsonfile.readFileSync(process.env.MAPPING)) {
-  const mapitem: any = {};
-  if (item.httpauth) {
-    mapitem.proxy = proxy.createProxy({ target: item.real, ws: true, xfwd: true, auth: item.httpauth });
-    console.log(`[${cluster.worker.id}] Loaded Route: ${item.host} > ${item.real} with Basic HttpAuth`);
-  } else {
-    mapitem.proxy = proxy.createProxy({ target: item.real, ws: true, xfwd: true });
-    console.log(`[${cluster.worker.id}] Loaded Route: ${item.host} > ${item.real}`);
+    const mapitem: any = {};
+    if (item.httpauth) {
+      mapitem.proxy = proxy.createProxy({ target: item.real, ws: true, xfwd: true, auth: item.httpauth });
+      console.log(`[${cluster.worker.id}] Loaded Route: ${item.host} > ${item.real} with Basic HttpAuth`);
+    } else {
+      mapitem.proxy = proxy.createProxy({ target: item.real, ws: true, xfwd: true });
+      console.log(`[${cluster.worker.id}] Loaded Route: ${item.host} > ${item.real}`);
+    }
+    mapping.set(item.host, mapitem);
   }
-  mapping.set(item.host, mapitem);
-}
 
-  app.all('*', (req, res, next) => {
-    if (mapping.has(req.hostname)) {
-      const mapitem = mapping.get(req.hostname);
-      mapitem.proxy.web(req, res);
-    } else {
-      defaultroute.web(req, res);
-    }
-  });
-  httpserver.on("upgrade", (req: http.IncomingMessage, socket, head) => {
-    const hostname = req.headers.host.split(":", 1)[0];
-    if (mapping.has(hostname)) {
-      mapping.get(hostname).proxy.ws(req, socket, head);
-    } else {
-      socket.destroy();
-    }
-  });
-
-  if (HTTPS_ENABLED) {
-    httpsserver.on("upgrade", (req: http.IncomingMessage, socket, head) => {
-      const hostname = req.headers.host.split(":", 1)[0];
-      if (mapping.has(hostname)) {
-        mapping.get(hostname).proxy.ws(req, socket, head);
-      } else {
-        socket.destroy();
+  function proxyWeb(req: express.Request, res: express.Response, next: express.NextFunction) {
+    for (const [host, mapitem] of mapping) {
+      if (req.hostname === host) {
+        mapitem.proxy.web(req, res);
+        return;
+      } else if (host.startsWith('*') && req.hostname.endsWith(host)) {
+        mapitem.proxy.web(req, res);
+        return;
       }
-    });
+    }
+    defaultProxy.web(req, res);
+    return;
   }
+
+  function proxyWS(req: http.IncomingMessage, socket, head) {
+    const hostname = req.headers.host.split(":", 1)[0];
+    for (const [host, mapitem] of mapping) {
+      if (hostname === host) {
+        mapitem.proxy.ws(req, socket, head);
+        return;
+      } else if (host.startsWith('*') && hostname.endsWith(host)) {
+        mapitem.proxy.ws(req, socket, head);
+        return;
+      }
+    }
+    socket.destroy();
+  }
+
+  app.all('*', proxyWeb);
+  httpserver.on("upgrade", proxyWS);
+  if (HTTPS_ENABLED) httpsserver.on('upgrade', proxyWS);
 
   httpserver.listen(8080);
   if (HTTPS_ENABLED) httpsserver.listen(8443);
